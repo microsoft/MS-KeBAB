8cc4df93afe448498703d60b956e09a671177734
allenwang <allenwang@microsoft.com>
2024-12-04 17:41:17 +0000

    Add eval_result_path

--- kebab/contracts/task.py
+++ kebab/contracts/task.py
@@ -91,12 +91,13 @@ class TaskInstance(ABC):
         return task_class(instance_name, task, **data_dict)
 
     @abstractmethod
-    def evaluate(self, output_to_evaluate: Path | None) -> dict[str, float]:
+    def evaluate(self, output_to_evaluate: Path, eval_result_path: Path | None = None) -> dict[str, float]:
         """
         Evaluate an output for the task instance.
 
         Args:
-        output_to_evaluate: Optional path to save the evaluation result.
+        output_to_evaluate: The output that needs to be evaluated.
+        eval_result_path: Optional path to save the evaluation result.
 
         Returns:
         dict[str, float]: A dictionary containing the evaluation metrics.
