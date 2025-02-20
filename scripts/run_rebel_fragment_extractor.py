125480022e638396ad1ac7ddd0d36fe87462e198
Pavel Myshkov <pmyshkov@microsoft.com>
2025-02-20 16:36:57 +0000

    Use full data by default

--- scripts/run_rebel_fragment_extractor.py
+++ scripts/run_rebel_fragment_extractor.py
@@ -22,8 +22,8 @@ from kebab.utils.dataset.rebel.rebel_fragment_extractor import RebelFragmentExtr
     / "REBEL"
     / "Original Dataset"
     # / "single",
-    / "sample",
-    # / "full",
+    # / "sample",
+    / "full",
     help="Path to the REBEL data directory (will be *json/*.jsonl-globbed against).",
 )
 @click.option(
