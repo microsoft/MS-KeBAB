c96db2557219b61bbc9c8fc821ea067cb019474e
atorunova <atorunova@microsoft.com>
2025-08-21 17:31:20 +0100

    fix md5 check; modify paths

--- scripts/dataset/download_and_preprocess_redocred.sh
+++ scripts/dataset/download_and_preprocess_redocred.sh
@@ -7,7 +7,8 @@ set -euo pipefail
 # Usage: ./download_and_preprocess_redocred.sh [-f]
 
 data_dir="./data"
-wikidata_output_dir="${data_dir}/Wikidata"
+date=$(date -u "+%Y-%m-%d")
+wikidata_output_dir="${data_dir}/Wikidata/${date}"
 redocred_output_dir="${data_dir}/Re-DocRED"
 mkdir -p "$wikidata_output_dir"
 wikidata_properties_file="wikidata_properties.json"
@@ -28,38 +29,46 @@ fi
 # Download the Redocred dataset
 echo "Downloading Redocred dataset..."
 for split in train dev test; do
-    mkdir -p "${redocred_output_dir}/${split}"
-    split_filename="${redocred_output_dir}/${split}/${split}.json"
-    if [ $regenerate_everything = "true" ] || [ ! -f $split_filename ]; then
+    mkdir -p "${redocred_output_dir}/Original Dataset/${split}"
+    split_filename="${redocred_output_dir}/Original Dataset/${split}/${split}.json"
+    if [ $regenerate_everything = "true" ] || [ ! -f "$split_filename" ]; then
         curl -X GET "https://raw.githubusercontent.com/tonytan48/Re-DocRED/refs/heads/main/data/${split}_revised.json" \
-        -o $split_filename
+        -o "$split_filename"
     else
         echo "File ${split_filename} already exists. Skipping download"
     fi
 done
-echo "Redocred dataset downloaded to ${redocred_output_dir}"
+echo "Redocred dataset downloaded to ${redocred_output_dir}/Original Dataset"
 
 # Preprocess the Redocred dataset
 echo "Preprocessing Redocred dataset..."
 for split in train dev test; do
     echo "Processing split: $split"
-    if [ $regenerate_everything = "true" ] || [ ! -d "${redocred_output_dir}/Extraction/${split}" ]; then
-        rm -rf "${redocred_output_dir}/Extraction/${split}"
-        mkdir -p "${redocred_output_dir}/Extraction/${split}"
+    extractions_path="${redocred_output_dir}/Extractions/${date}/${split}"
+    if [ $regenerate_everything = "true" ] || [ ! -d $extractions_path ]; then
+        rm -rf $extractions_path
+        mkdir -p $extractions_path
     else
         echo "Extraction directory for ${split} already exists. Skipping preprocessing."
         continue
     fi
     uv run ./scripts/dataset/run_re_docred_dataset_builder.py \
-    --re-docred-dir "${redocred_output_dir}/${split}" \
+    --re-docred-dir "${redocred_output_dir}/Original Dataset/${split}" \
     --wikidata-properties-path "$wikidata_properties_path" \
-    --output-dir "${redocred_output_dir}/Extraction/${split}"
+    --output-dir "${extractions_path}"
 done
 
 echo "Checking md5 of generated data..."
-if md5sum -c ${data_dir}/Re-DocRED/extraction.md5 --status; then
-    echo "MD5 check passed. All files are correctly generated."
-else
-    echo "MD5 check failed. Please check the generated files."
-    exit 1
-fi
+while IFS= read -r line || [[ -n "$line" ]]; do
+    file=$(echo "$line" | awk '{print $2}')
+    file_path=${redocred_output_dir}/Extractions/${date}/${file}
+    expected_md5=$(echo "$line" | awk '{print $1}')
+    actual_md5=$(tr -d "\r" < $file_path | md5sum | awk '{print $1}')
+
+    if [ "$actual_md5" != "$expected_md5" ]; then
+        echo "MD5 mismatch for ${file_path}: expected ${expected_md5}, got ${actual_md5}"
+        exit 1
+    fi
+done < "${data_dir}/Re-DocRED/extraction.md5"
+
+echo "MD5 check passed. All files are correctly generated."
