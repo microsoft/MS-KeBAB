0bf4fea792d52ebc3dd4bfcb88f15fa29df0f4fb
Tom Minka <8955276+tminka@users.noreply.github.com>
2025-12-15 16:12:47 +0000

    merge_linking_predictions.py also merges metrics files.

--- scripts/metrics/merge_linking_predictions.py
+++ scripts/metrics/merge_linking_predictions.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 
 def safe_merge(df1: pd.DataFrame, df2: pd.DataFrame, on: list[str]) -> pd.DataFrame:
+    """Merge two dataframes on specified columns, with error handling for missing keys."""
     try:
         return df1.merge(df2, on=on)
     except KeyError:
@@ -31,6 +32,7 @@ def main():
     The merged file will be named 'merged_linking_predictions.tsv'.
     Each system's columns will be suffixed with the folder name, excluding the common prefix.
     If a tsv file has a suffix in its name (after 'linking_predictions'), that suffix will also be added.
+    All metrics*.json files will also be merged into 'merged_metrics.tsv' similarly.
     """
     )
     parser.add_argument("folder", nargs="+", type=Path, help="folder to merge")
@@ -40,8 +42,8 @@ def main():
     folders = [p for folder in folders for p in glob.glob(str(folder)) if Path(p).is_dir()]
     # Find the common prefix of all folder names
     common_prefix = Path(os.path.commonprefix(folders))
-    output_filename = "merged_linking_predictions.tsv"
     merged_data_frame = None
+    merged_metrics = None
     system_columns = ["log_odds", "predicted_label", "debugging_info", "debugging_info.1", "debugging_info.2"]
     for folder in folders:
         folder_path = Path(folder)
@@ -59,8 +61,19 @@ def main():
             # Add suffix to all system_columns
             df = df.rename(columns={c: c + suffix for c in system_columns})
             merged_data_frame = df if merged_data_frame is None else safe_merge(merged_data_frame, df, on=join_columns)
+        for metrics_file in folder_path.glob("metrics*.json"):
+            suffix = folder_suffix + metrics_file.stem.replace("metrics", "")
+            print(f"  Reading file {metrics_file} with suffix '{suffix}'")
+            df = pd.read_json(metrics_file, typ="series").to_frame().T.set_index(pd.Index([suffix]))
+            merged_metrics = df if merged_metrics is None else pd.concat([merged_metrics, df])
+
+    if merged_metrics is not None:
+        merged_metrics_filename = "merged_metrics.tsv"
+        merged_metrics.to_csv(merged_metrics_filename, sep="\t", index=True)
+        print(f"Finished merging metrics files into {merged_metrics_filename}")
 
     if merged_data_frame is not None:
+        output_filename = "merged_linking_predictions.tsv"
         merged_data_frame.to_csv(output_filename, sep="\t", index=False)
         print(f"Finished merging files into {output_filename}")
 
